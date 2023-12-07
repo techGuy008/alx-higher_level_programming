81a711946e1588092de82b9f474bcff48ab5afd7
Amine S <a.saroui21@gmail.com>
2023-12-07 02:41:20 +0100

    update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,61 +1,29 @@
 #!/usr/bin/python3
-thousands = ["MMM", "MM", "M"]
-hundreds = ["CM", "DCCC", "DCC", "DC", "D",
-            "CD", "CCC", "CC", "C"]
-tens = ["XC", "LXXX", "LXX", "LX", "L",
-        "XL", "XXX", "XX", "X"]
-digits = ["IX", "VIII", "VII", "VI", "V",
-          "IV", "III", "II", "I"]
+def roman_intVal(r):
+    dict = {
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000
+    }
+    return dict[r] if r in dict else -1 
 
-
-def check_digit(string, nums):
-    for i in nums:
-        if i in string:
-            return list(reversed(nums)).index(i) + 1
-    return -1
-
-
-def convert_roman_to_int(string):
-    thousand = check_digit(string, thousands)
-    if thousand != -1:
-        delete = thousands[thousand - 1]
-        string = string.replace(delete, '')
-    else:
-        thousand = 0
-    hundred = check_digit(string, hundreds)
-    if hundred != -1:
-        delete = hundreds[hundred - 1]
-        string = string.replace(delete, '')
-    else:
-        hundred = 0
-    ten = check_digit(string, tens)
-    if ten != -1:
-        delete = tens[ten - 1]
-        string = string.replace(delete, '')
-    else:
-        ten = 0
-    digit = check_digit(string, digits)
-    if digit != -1:
-        delete = digits[digit - 1]
-        string = string.replace(delete, '')
-    else:
-        digit = 0
-    return thousand * 1000 + hundred * 100 + ten * 10 + digit
-
-
-def check_input(string):
-    for i in string:
-        if i not in 'MCDLXVI':
-            return False
-    return True
-
-
-def roman_to_int(roman_string):
-    if roman_string is None:
-        return 0
-    if not isinstance(roman_string, str):
-        return 0
-    if not check_input(roman_string):
-        return 0
-    result = convert_roman_to_int(roman_string)
-    return result
+def roman_to_decimal(str):
+    res = 0
+    i = 0
+    while i < len(str):
+        str1 = roman_intVal(str[i])
+        if i + 1 < len(str):
+            str2 = roman_intVal(str[i + 1])
+            if str1 >= str2:
+                res += str1
+            else:
+                res += str2 - str1
+                i += 1
+        else:
+            res += str1
+        i += 1
+    return res
